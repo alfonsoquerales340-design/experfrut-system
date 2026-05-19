9a5ae0b2da87fe197a75064779d508a147c58842
alfonsoquerales340-design <alfonsoquerales340@gmail.com>
2026-05-19 00:46:52 -0400

    Refactor settings.py for improved configuration

--- experfrut_project/settings.py
+++ experfrut_project/settings.py
@@ -104,7 +104,7 @@ TEMPLATES = [
 # ==============================================================================
 DATABASES = {
     'default': dj_database_url.config(
-        default= 'sqlite:///db.sqlite3',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
