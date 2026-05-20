5051845a44e4184228d8360489d46e0dce26dd1e
alfonsoquerales340-design <alfonsoquerales340@gmail.com>
2026-05-20 09:34:03 -0400

    "Configurar instalacion en build.sh"
    Added error handling to the build script.

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-# Instalar dependencias de Python
+# 1. Instalar las dependencias del archivo requirements.txt
 pip install -r requirements.txt
 
-# Recopilar archivos estáticos (CSS, JS, iconos de la PWA)
+# 2. Recopilar los archivos estáticos
 python manage.py collectstatic --no-input
 
-# Aplicar las migraciones a tu base de datos demo
+# 3. Aplicar las migraciones de la base de datos
 python manage.py migrate
