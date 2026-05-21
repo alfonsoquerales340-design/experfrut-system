91ec3b4dafba0dc362980df9b38f6330a7e77ded
alfonsoquerales340-design <alfonsoquerales340@gmail.com>
2026-05-20 22:43:13 -0400

    Add command to create superuser in build script

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ pip install -r requirements.txt
 
 # 2. Recopilar los archivos estáticos
 python manage.py collectstatic --no-input
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); Us
 
 
