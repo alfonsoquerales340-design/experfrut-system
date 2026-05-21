33e367dea7f4a09cd91531ba769ddaed9d2aab46
alfonsoquerales340-design <alfonsoquerales340@gmail.com>
2026-05-20 22:45:31 -0400

    Update build script for dependency installation and superuser

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 # exit on error
 set -o errexit
 
-# 1. Instalar las dependencias del archivo requirements.txt
+# 1. Instalar las dependencias
 pip install -r requirements.txt
 
 # 2. Recopilar los archivos estáticos
 python manage.py collectstatic --no-input
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); Us
+
+# 3. Crear el superusuario automáticamente si no existe
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'tu_correo@gmail.com', 'Experfrut2026')"
 
 
