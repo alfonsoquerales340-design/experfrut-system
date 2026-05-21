39ea8f0391177273ed3141ae7ca6f53820033fe1
alfonsoquerales340-design <alfonsoquerales340@gmail.com>
2026-05-20 22:52:49 -0400

    Clean up build.sh by removing comments and lines
    Removed comments and unnecessary lines from build script.

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# 1. Instalar las dependencias
 pip install -r requirements.txt
-
-# 2. Recopilar los archivos estáticos
 python manage.py collectstatic --no-input
-
-# 3. Crear el superusuario automáticamente si no existe
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'tu_correo@gmail.com', 'Experfrut2026')"
 
 
