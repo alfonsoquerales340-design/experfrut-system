0593883a7728ec276884a3dd1d82886384e77784
alfonsoquerales340-design <alfonsoquerales340@gmail.com>
2026-05-24 15:42:13 -0400

    Update urls.py

--- experfrut_project/urls.py
+++ experfrut_project/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from two_factor.urls import urlpatterns as tf_urls
 
-from apps.views import index, registrar_salida, dashboard_vendas, analista_ia, ai_test, dashboard_avanzado
+# Cambia 'from apps.views import ...' por esto:
+from views import index, registrar_salida, dashboard_vendas, analista_ia, ai_test, dashboard_avanzado
 
 urlpatterns = [
     # 1. Admin (Jazzmin se acopla aquí)
