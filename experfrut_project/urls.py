a7aa941bf101791f6b926f488928064deed2493b
alfonsoquerales340-design <alfonsoquerales340@gmail.com>
2026-05-24 12:45:19 -0400

    Refactor URL imports and add static URL handling

--- experfrut_project/urls.py
+++ experfrut_project/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from two_factor.urls import urlpatterns as tf_urls
 
-# Importamos tus vistas de tienda (Agregamos dashboard_avanzado)
-from tienda.views import index, registrar_salida, dashboard_vendas, analista_ia, ai_test, dashboard_avanzado
+# Importamos tus vistas directamente desde la raíz (Quitamos "tienda.")
+from views import index, registrar_salida, dashboard_vendas, analista_ia, ai_test, dashboard_avanzado
 
 urlpatterns = [
     # 1. Admin
