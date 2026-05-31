ab27d06c9c6194f2b46bc383a0d4c29720816975
alfonsoquerales340-design <alfonsoquerales340@gmail.com>
2026-05-31 12:34:00 -0400

    Update wsgi.py

--- experfrut_project/wsgi.py
+++ experfrut_project/wsgi.py
@@ -1,12 +1,14 @@
-
 import os
 import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# Añadir la raíz al camino de Python para que encuentre 'app'
+# Calculamos la raíz real del proyecto (donde están manage.py y tienda)
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Inyectamos las rutas de forma absoluta en el sistema de Python
 sys.path.append(str(BASE_DIR))
+sys.path.append(str(BASE_DIR / 'tienda'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'experfrut_project.settings')
 
