7b301974183bb53852bec0a1ab52016fc74b79ba
alfonsoquerales340-design <alfonsoquerales340@gmail.com>
2026-06-02 23:37:45 -0400

    Update wsgi.py

--- experfrut_project/wsgi.py
+++ experfrut_project/wsgi.py
@@ -12,3 +12,13 @@ sys.path.append(str(BASE_DIR))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'experfrut_project.settings')
 
 application = get_wsgi_application()
+# SCRIPT TEMPORAL PARA CREAR TU ADMINISTRADOR EN PRODUCCIÓN
+from django.contrib.auth import get_user_model
+try:
+    User = get_user_model()
+    # Configurado para Alfonso con credenciales de producción de Experfrut
+    if not User.objects.filter(username='alfonso').exists():
+        User.objects.create_superuser('alfonso', 'alfonso@experfrut.com', 'alfa18#')
+        print("¡Superusuario de producción creado con éxito!")
+except Exception as e:
+    print(f"Nota del superusuario: {e}")
